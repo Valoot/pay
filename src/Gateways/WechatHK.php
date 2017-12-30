9db2ad279ea62c252132e44d241853cfd44997b7
Ryan Chan <rc@riseno.com>
2017-12-30 18:50:56 +0800

    added wechat hk gateways

---
+++ src/Gateways/WechatHK.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryanchan
+ * Date: 30/12/2017
+ * Time: 6:42 PM
+ */
+
+namespace Yansongda\Pay\Gateways;
+
+
+use Yansongda\Pay\Contracts\GatewayApplicationInterface;
+use Yansongda\Supports\Config;
+
+class WechatHK extends Wechat implements GatewayApplicationInterface
+{
+    /**
+     * WechatHK constructor.
+     * @param Config $config
+     */
+    public function __construct(Config $config)
+    {
+        parent::__construct($config);
+
+        $this->gateway = 'https://mchapi.wechatpay.com.hk/cgi-bin/';
+    }
+}
---
+++ src/Gateways/WechatHK/PosGateway.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryanchan
+ * Date: 30/12/2017
+ * Time: 6:46 PM
+ */
+
+namespace Yansongda\Pay\Gateways\WechatHK;
+
+
+class PosGateway
+{
+
+}
