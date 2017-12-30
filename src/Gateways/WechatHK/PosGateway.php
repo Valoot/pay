9c6f2be151d23a6ed93ec5e0e03f16c33301bf04
Ryan Chan <rc@riseno.com>
2017-12-30 18:51:18 +0800

    added wechat hk pos

--- src/Gateways/WechatHK/PosGateway.php
+++ src/Gateways/WechatHK/PosGateway.php
@@ -9,7 +9,21 @@
 namespace Yansongda\Pay\Gateways\WechatHK;
 
 
-class PosGateway
+use Yansongda\Supports\Collection;
+
+class PosGateway extends \Yansongda\Pay\Gateways\Wechat\PosGateway
 {
+    /**
+     * @param string $endpoint
+     * @param array $payload
+     *
+     * @return Collection
+     */
+    public function pay($endpoint, array $payload): Collection
+    {
+        unset($payload['trade_type'], $payload['notify_url']);
+
+        return $this->preOrder('ia_mch_qr_auth.cgi', $payload);
+    }
 
 }
